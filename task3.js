93d6d21f15622b5abb16d35a463661573e43274f
ajith8699 <ajithkumara@thestackly.com>
2026-06-02 01:07:50 +0530

    task3

---
+++ task3.js
@@ -0,0 +1,252 @@
+// 1. Student Login Check
+let username = prompt("Enter username:");
+let password = prompt("Enter password:");
+
+if (username === "admin" && password === "2580") {
+    console.log("Login Success");
+} else {
+    console.log("Invalid User");
+}
+
+
+// 2. ATM Balance System
+let balance = 4000;
+let amount = Number(prompt("Enter withdraw amount:"));
+
+if (amount < balance) {
+    console.log("Withdraw Success");
+} else {
+    console.log("Insufficient Balance");
+}
+
+
+// 3. Traffic Signal Project
+let signal = prompt("Enter signal color:");
+
+switch (signal) {
+    case "red":
+        console.log("Stop");
+        break;
+
+    case "yellow":
+        console.log("Ready");
+        break;
+
+    case "green":
+        console.log("Go");
+        break;
+
+    default:
+        console.log("Invalid Signal");
+}
+
+
+// 4. Age Eligibility Checker
+let age = Number(prompt("Enter your age:"));
+
+if (age >= 18) {
+    console.log("Eligible for Vote");
+} else {
+    console.log("Not Eligible");
+}
+
+
+// 5. Shopping Discount System
+
+let amount = Number(prompt("Enter shopping amount:"));
+
+if (amount >= 5000) {
+    console.log("You got 20% discount");
+} else if (amount >= 3000) {
+    console.log("You got 10% discount");
+} else if (amount >= 1000) {
+    console.log("You got 5% discount");
+} else {
+    console.log("No discount available");
+}
+
+// 6. Password Strength Checker
+let pass = prompt("Enter password:");
+
+if (pass.length >= 8) {
+    console.log("Strong Password");
+} else {
+    console.log("Weak Password");
+}
+
+
+// 7. Mobile Battery Warning
+let battery = Number(prompt("Enter battery percentage:"));
+
+if (battery <= 10) {
+    console.log("Battery Low");
+} else if (battery <= 50) {
+    console.log("Battery Medium");
+} else {
+    console.log("Battery Full");
+}
+
+
+// 8. Employee Salary Bonus
+let salary = 30000;
+let bonus;
+
+if (salary >= 50000) {
+    bonus = salary * 0.15;
+} else if (salary >= 30000) {
+    bonus = salary * 0.10;
+} else {
+    bonus = salary * 0.05;
+}
+
+console.log("Bonus:", bonus);
+
+
+// 9. Online Food Order
+let orderAmount = Number(prompt("Enter order amount:"));
+
+if (orderAmount > 599) {
+    console.log("Free Delivery");
+} else {
+    console.log("Delivery Charge Added");
+}
+
+
+// 10. Movie Ticket Booking
+let movieAge = Number(prompt("Enter age:"));
+
+if (movieAge < 5) {
+    console.log("Free Ticket");
+} else if (movieAge < 18) {
+    console.log("Child Ticket");
+} else {
+    console.log("Adult Ticket");
+}
+
+
+// 11. Instagram Likes Counter
+// using for loop
+//print:
+//LIKE 1
+//LIKE 2
+//LIKE 3
+//LIKE 4
+//LIKE 5
+for (let i = 1; i <= 5; i++) {
+    console.log("Like", i);
+}
+
+
+// 12. Countdown Timer
+// using while loop
+//Print:
+//5
+//4
+//3
+//2
+//1 
+let count = 5;
+
+while (count >= 1) {
+    console.log(count);
+    count--;
+}
+
+console.log("Start");
+
+
+// 13. OTP Verification
+let OTP = 2580;
+let userOTP = Number(prompt("Enter OTP:"));
+
+if (userOTP === OTP) {
+    console.log("OTP Verified");
+} else {
+    console.log("Invalid OTP");
+}
+
+
+// 14. E-commerce Cart Total
+let cart = [200, 500, 1000, 300];
+let total = 0;
+
+for (let item of cart) {
+    total += item;
+}
+
+console.log("Cart Total:", total);
+
+
+// 15. Student Mark Grade System
+let mark = Number(prompt("Enter marks:"));
+
+if (mark >= 90) {
+    console.log("A Grade");
+} else if (mark >= 70) {
+    console.log("B Grade");
+} else if (mark >= 50) {
+    console.log("C Grade");
+} else {
+    console.log("Fail");
+}
+
+
+// 16. Netflix Subscription Checker
+let subscription = true;
+
+if (subscription) {
+    console.log("Watch Movie");
+} else {
+    console.log("Buy Subscription");
+}
+
+
+// 17. Product Stock Checker
+let stock = 0;
+
+if (stock > 0) {
+    console.log("Product Available");
+} else {
+    console.log("Out of Stock");
+}
+
+
+// 18. Weather App Condition
+let weather = prompt("Enter weather:");
+
+switch (weather) {
+    case "sunny":
+        console.log("Go Outside");
+        break;
+
+    case "rainy":
+        console.log("Take Umbrella");
+        break;
+
+    case "cold":
+        console.log("Wear Jacket");
+        break;
+
+    default:
+        console.log("Invalid Weather");
+}
+
+
+// 19. User Profile Object
+let user = {
+    name: "abids",
+    age: 20,
+    city: "Hyderabad"
+};
+
+for (let key in user) {
+    console.log(key + ":", user[key]);
+}
+
+
+// 20. WhatsApp Chat Array
+let chats = ["hi", "hello", "where are you", "ok"];
+
+for (let message of chats) {
+    console.log(message);
+}
